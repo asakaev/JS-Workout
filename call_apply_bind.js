97e1a56732bc152bb29e9473b3f52014acabc9f1
tema <tema@vf8.ru>
2015-04-06 03:10:11 +0300

    call mouse method in cat context

---
+++ call_apply_bind.js
@@ -0,0 +1,11 @@
+var cat = { name: 'Tom'};
+var mouse = { name: 'Jerry'};
+
+mouse.sayName = function () {
+    return this.name;
+};
+
+console.log('Mouse name: ' + mouse.sayName());
+
+var catName = mouse.sayName.call(cat);
+console.log('Cat name: ' + catName);
