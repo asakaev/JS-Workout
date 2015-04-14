ff8bdde04cc1c3421d5b98f17d567e5ecd3e4b25
tema <tema@vf8.ru>
2015-04-14 19:20:31 +0300

    Simplified

--- stack.js
+++ stack.js
@@ -1,18 +1,14 @@
 // If comment 1st console.log then unusual behavor in catch
 // If not comment you can't get anything inside catch
 
-(function f(args)  {
-   var self=this;
-   var k=args.k;
-   console.log(k);
+(function f(n) {
+  console.log(n);
 
-   try
-   {
-       f({k:k+1});
-   }
-   catch(e)
-   {
-        // console.log(k);
-        // process.stdout.write('k: ' + k);
-   }
-})({k:0});
+  try {
+    f(++n);
+  }
+  catch (e) {
+    // console.log(n);
+    // process.stdout.write('n: ' + n);
+  }
+})(0);
