eb7b952d9f0a5dc3a3f0fdc566d139ecabf49e78
Akhtyam Sakaev <tema@vf8.ru>
2015-09-27 23:42:57 +0300

    Async loop using closure

---
+++ closure.js
@@ -0,0 +1,17 @@
+for (var i = 0; i < 5; i++) {
+  console.log('Inside loop', i);
+
+  /**
+   * Function that save context for i variable
+   * @param {Number} i
+   */
+  function f(i) {
+    setTimeout(function () {
+      console.log('Timeout', i);
+    }, 1000);
+  }
+
+  f(i);
+}
+
+console.log('After loop:', i);
