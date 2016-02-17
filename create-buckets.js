0888154c4cb400ba387c723a2d3bf6aaefcd35c9
Akhtyam Sakaev <tema@vf8.ru>
2016-02-17 12:09:49 +0300

    Buckets problem

---
+++ create-buckets.js
@@ -0,0 +1,70 @@
+
+
+/**
+ * Return array of buckets
+ * @param {Array} array
+ * @param {string} field
+ * @param {number} bucketSize
+ * @return {Array}
+ */
+function createBuckets(array, field, bucketSize) {
+  var result = [];
+
+  var bucket = [];
+  var currentBucketSize = 0;
+
+  while (array.length) {
+    var arrayElement = array.shift();
+    var item = arrayElement[field];
+
+    var correctItem = Array.isArray(item) && item.length <= bucketSize;
+    if (!correctItem) {
+      continue;
+    }
+
+    currentBucketSize += item.length;
+    if (currentBucketSize <= bucketSize) {
+      bucket.push(arrayElement);
+    } else {
+      result.push(bucket);
+
+      bucket = [];
+      currentBucketSize = item.length;
+      bucket.push(arrayElement);
+    }
+  }
+
+  result.push(bucket);
+  return result;
+}
+
+
+var countries = [
+  {
+    country: 'Russia',
+    cities: ['Saint Petersburg', 'Moscow', 'Novosibirsk', 'Kazan']
+  },
+  {
+    country: 'Ukraine',
+    cities: [1, 2, 3, 4, 5, 6, 7, 8]
+  },
+  {
+    country: 'Kazakhstan',
+    cities: [1, 2, 3, 4, 5]
+  },
+  {
+    country: 'Azerbaijan',
+    cities: [1, 2]
+  }
+];
+
+var maxBucketSize = 12;
+var field = 'cities';
+
+var arrayOfBuckets = createBuckets(countries, field, maxBucketSize);
+
+//[ [ { country: 'Russia', cities: [Object] },
+//  { country: 'Ukraine', cities: [Object] } ],
+//  [ { country: 'Kazakhstan', cities: [Object] },
+//    { country: 'Azerbaijan', cities: [Object] } ] ]
+console.log(arrayOfBuckets);
