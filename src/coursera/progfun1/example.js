f900bb0683f2bc3d37bc89dbd973c8c62d6333e7
Akhtyam Sakaev <tema@vf8.ru>
2017-04-12 21:20:32 +0300

    types

--- src/coursera/progfun1/example.js
+++ src/coursera/progfun1/example.js
@@ -1,20 +1,36 @@
 
+/**
+ * @param {Number} x
+ * @returns {Number}
+ */
 function intNotZero(x) {
   if (x === 0) throw new Error('zero is not allowed')
   else return x
 }
 
-
+/**
+ * @param {Array} list
+ * @returns {Number}
+ */
 function sum(list) {
   return !list.length ? 0 : list.shift() + sum(list)
 }
 
+/**
+ * @param {Array} list
+ * @returns {Number}
+ */
 function max(list) {
   if (!list.length) throw new Error('NoSuchElementException')
   return list.length > 1 ? maxAux(list.shift(), max(list)) : list.shift()
 
 }
 
+/**
+ * @param {Number} a
+ * @param {Number} b
+ * @returns {Number}
+ */
 function maxAux(a, b) {
   return a > b ? a : b
 }
