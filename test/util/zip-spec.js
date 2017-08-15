3ebc37312af9edd8d53de0c4d8c5554710996ddf
Akhtyam Sakaev <tema@vf8.ru>
2017-08-15 15:26:44 +0300

    zip

---
+++ test/util/zip-spec.js
@@ -0,0 +1,46 @@
+const mocha = require('mocha')
+const suite = mocha.suite
+const test = mocha.test
+const assert = require('assert')
+
+const zip = require('../../src/util/zip').zip
+
+
+suite('ZipSuite', function() {
+
+  test('collections with same size', function() {
+    const xs1 = [1, 2, 3]
+    const xs2 = [4, 5, 6]
+    const expected = [[1, 4], [2, 5], [3, 6]]
+    assert.deepEqual(zip(xs1, xs2), expected)
+  })
+
+  test('both empty collections', function() {
+    const xs1 = []
+    const xs2 = []
+    const expected = []
+    assert.deepEqual(zip(xs1, xs2), expected)
+  })
+
+  test('first empty, second non empty', function() {
+    const xs1 = [1, 2, 3]
+    const xs2 = []
+    const expected = []
+    assert.deepEqual(zip(xs1, xs2), expected)
+  })
+
+  test('first non empty, second empty', function() {
+    const xs1 = []
+    const xs2 = [1, 2, 3]
+    const expected = []
+    assert.deepEqual(zip(xs1, xs2), expected)
+  })
+
+  test('different size', function() {
+    const xs1 = [1, 2, 3, 4]
+    const xs2 = [5, 6]
+    const expected = [[1, 5], [2, 6]]
+    assert.deepEqual(zip(xs1, xs2), expected)
+  })
+
+})
